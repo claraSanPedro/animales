05ff34f5c40080b8434d6bd2d5c0389b96c630eb
baffy <baffy@correo.ro>
2022-03-30 20:27:07 +0200

    Pez olvidado

---
+++ src/animalitos/Pez.java
@@ -0,0 +1,14 @@
+package animalitos;
+
+public class Pez extends Mascota{
+	private String color;
+
+	public Pez(String nombre, String color) {
+		super(nombre);
+		this.color = color;
+	}
+
+	public void MandarMensaje() {
+		System.out.println("Soy " + this.getNombre() + " y GLU-GLU-GLU");
+	}
+}
