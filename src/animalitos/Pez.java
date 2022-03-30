70e0d23bb38837d1deab502d2acfe293874eacdc
javier <javier@correo.es>
2022-03-30 20:19:17 +0200

    Version 2 AnimalesGIT

---
+++ src/animalitos/Pez.java
@@ -0,0 +1,14 @@
+package animalitos;
+
+public class Pez extends Mascota{
+
+	public Pez(String nombre) {
+		super(nombre);
+		
+	}
+	
+	public void MandarMensaje() {
+		System.out.println("Soy " + this.getNombre() + " y maúllo");
+	}
+
+}
