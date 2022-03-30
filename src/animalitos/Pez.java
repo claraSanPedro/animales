2c053455c2fa5869df09d81b1a0e15a37ce16567
Amanda <amandaroblesurena@gmail.com>
2022-03-30 20:21:45 +0200

    Primer commit Amanda

---
+++ src/animalitos/Pez.java
@@ -0,0 +1,15 @@
+package animalitos;
+
+public class Pez extends Mascota {
+	
+	private String raza;
+
+	public Pez(String nombre, String raza) {
+		super(nombre);
+		this.raza=raza;
+	}
+
+	public void MandarMensaje() {
+		System.out.println("Soy un pez");
+	}
+}
--- src/animalitos/Principal.java
+++ src/animalitos/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
 		Perro miPerro=new Perro("Lemmy","Braco");
 		Gato miGato=new Gato("Lily","Siamés");
 		Tortuga miTortuga=new Tortuga("Steven","Plana");
+		Pez miPez=new Pez("Nemo","Pez payaso");
 		
 		miPerro.MandarMensaje();
 		miGato.MandarMensaje();
