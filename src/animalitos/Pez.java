fe2a83b3f0188d1406e1fe2caca4b00f21d6b0bd
davisroa <davisroa@correo.es>
2022-03-30 20:25:33 +0200

    Commit con las clase Pez creada

---
+++ src/animalitos/Pez.java
@@ -0,0 +1,18 @@
+package animalitos;
+
+public class Pez extends Mascota{
+
+	public Pez(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void MandarMensaje() {
+		System.out.println("Soy " + this.getNombre() + " y nado glu glu");
+	}
+
+	
+	
+	
+
+}
--- src/animalitos/Principal.java
+++ src/animalitos/Principal.java
@@ -7,13 +7,15 @@ public class Principal {
 		Perro miPerro=new Perro("Lemmy","Braco");
 		Gato miGato=new Gato("Lily","Siamés");
 		Tortuga miTortuga=new Tortuga("Steven","Plana");
+		Pez miPez = new Pez("Blastoise");
 		
 		miPerro.MandarMensaje();
 		miGato.MandarMensaje();
 		miTortuga.MandarMensaje();
+		miPez.MandarMensaje();
 
 	
-
+		
 	}
 
 }
