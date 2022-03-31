e3e8f1b76dab8f2a63bc5e7999007d60a61ed6ac
Jorge Sánchez <jorge.sanchez1376@gmail.com>
2022-03-31 17:48:52 +0200

    Este es un commit

--- src/animalitos/Principal.java
+++ src/animalitos/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 		Perro miPerro=new Perro("Lemmy","Braco");
 		Gato miGato=new Gato("Lily","Siamés");
 		Tortuga miTortuga=new Tortuga("Steven","Plana");
-		
+		Pez miPez = new Pez("Liberto");
 		miPerro.MandarMensaje();
 		miGato.MandarMensaje();
 		miTortuga.MandarMensaje();
