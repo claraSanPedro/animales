5137c48fb8685d3e12e0510cb712497dca07331f
Jorge Sánchez <jorge.sanchez1376@gmail.com>
2022-03-31 17:48:42 +0200

    Este es un commit

---
+++ src/animalitos/Pez.java
@@ -0,0 +1,16 @@
+package animalitos;
+
+public class Pez extends Mascota{
+
+	public Pez(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Soy Pez y hago glu glu";
+	}
+	
+
+}
